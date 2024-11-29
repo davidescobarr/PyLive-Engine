9fa536b9d6b5eb542b80421e14118f8f9acabc94
DavidEscobarr <47315634+davidescobarr@users.noreply.github.com>
2024-11-29 15:23:45 +0300

    Fix not relative path to lang

--- engine/ui/lang/TextTranslater.py
+++ engine/ui/lang/TextTranslater.py
@@ -71,7 +71,7 @@ class TextTranslater:
         """Get a list of JSON files in the specified directory."""
         return glob(os.path.join(directory, '*.lang'))
 
-text_translator = TextTranslater("C:/Users/ender/Documents/Python/PyLive-Engine/engine/ui/lang")
+text_translator = TextTranslater("engine/ui/lang")
 lang = TextTranslater.get_language_by_code(global_settings.lang)
 if lang is not None:
     text_translator.switch_language(lang)
